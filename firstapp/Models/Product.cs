e6a81762a31ede59338ccf116ca78a07d5f90cad
samyi <samyi@DESKTOP-UL5L3S8>
2024-03-24 21:30:09 +0500

    Add project files.

---
+++ firstapp/Models/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace firstapp.Models;
+
+public partial class Product
+{
+    public int PId { get; set; }
+
+    public string? PName { get; set; }
+
+    public int? PPrice { get; set; }
+
+    public string? PDiscription { get; set; }
+
+    public string? PCategory { get; set; }
+}
